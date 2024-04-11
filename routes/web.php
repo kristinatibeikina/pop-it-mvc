a56876953d24c95c73da96e287f3452b0f42aa62
kristinatibeikina <kristina.tibeykina04@mail.ru>
2024-04-11 23:18:44 +0700

    Страница создания пользователей

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ use Src\Route;
 Route::add('GET', '/hello', [Controller\Site::class, 'hello'])->middleware('auth');
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add(['GET','POST'], '/logout', [Controller\Site::class, 'logout']);
-Route::add(['GET', 'POST'], '/signup', [Controller\Site::class, 'signup'])->middleware('admin');
+Route::add(['GET', 'POST'], '/signup', [Controller\Admin::class, 'signup'])->middleware('admin');
---
+++ views/admin/signup.php
@@ -0,0 +1,15 @@
+<div class="create_user">
+    <h1 class="title_login">Добавление</h1>
+    <h2 class="add_title_dv">Новые сотрудники</h2>
+    <form method="post" class="form_login">
+        <input type="text" name="name" placeholder="Имя" class="input_login"><br>
+        <input type="text" name="login" placeholder="Логин" class="input_password"><br>
+        <input type="password" name="password" placeholder="Пароль" class="input_password"><br>
+        <select name="role" class="role">
+            <option selected="selected">Роли</option>
+            <option>Сотрудник</option>
+            <option>Администратор</option>
+        </select><br>
+        <button class="btn">Отправить</button>
+    </form>
+</div>
