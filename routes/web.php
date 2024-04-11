04a038427273039fda2e706d77438456f7d7f404
kristinatibeikina <kristina.tibeykina04@mail.ru>
2024-04-11 23:25:38 +0700

    Страница пользователя добавления зданий

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login']);
 Route::add(['GET','POST'], '/logout', [Controller\Site::class, 'logout']);
 Route::add(['GET', 'POST'], '/signup', [Controller\Admin::class, 'signup'])->middleware('admin');
 Route::add(['GET', 'POST'], '/counting', [Controller\Employees::class, 'counting'])->middleware('employees');
+Route::add(['GET', 'POST'], '/addendum', [Controller\Employees::class, 'addendum'])->middleware('employees');
---
+++ views/employees/addendum.php
@@ -0,0 +1,21 @@
+<div class="add">
+    <div class="add_bloc">
+        <h1 class="add_title">Добавление</h1>
+        <h2 class="add_title_dv">Новые здания</h2>
+        <form method="post" class="form_add ">
+            <input type="text" name="title" placeholder="Название" class="input_login"><br>
+            <input type="text" name="Address" placeholder="Адрес" class="input_password"><br>
+            <button class="btn">Отправить</button>
+        </form>
+        <h2 class="add_title_dv">Новые помещения</h2>
+        <form method="post" class="form_add_room ">
+            <input type="te" name="title" placeholder="Здание" class="input_login"><br>
+            <input type="title" name="adres" placeholder="Название или номер помещения" class="input_password"><br>
+            <input type="password" name="adres" placeholder="Вид помещения" class="input_password"><br>
+            <input type="password" name="adres" placeholder="Площадь" class="input_password"><br>
+            <input type="password" name="adres" placeholder="Кол-во посадочных мест" class="input_password"><br>
+            <button class="btn">Отправить</button>
+        </form>
+    </div>
+
+</div>
