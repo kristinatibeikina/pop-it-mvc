16440691f26849ad671d2aa9b549a711f303e05c
kristinatibeikina <kristina.tibeykina04@mail.ru>
2024-04-11 23:24:03 +0700

    Страница пользователя для подсчета

---
+++ views/employees/counting.php
@@ -0,0 +1,33 @@
+<div class="counting">
+    <div class="counting_bloc">
+        <h1 class="add_title">Подсчет</h1>
+        <h2 class="add_title_dv">Кол-во посадочных мест по зданиям</h2>
+        <form method="post" class="form_add ">
+            <select name="role" class="role">
+                <option selected="selected">Здание</option>
+                <option>Сотрудник</option>
+                <option>Администратор</option>
+            </select><br>
+            <button class="btn">Расчитать</button><br>
+            <input type="text" name="title" placeholder="Колличество" class="input_login response"><br>
+
+        </form>
+        <h2 class="add_title_dv">Площадь аудиторий по зданиям</h2>
+        <form method="post" class="form_add ">
+            <select name="role" class="role">
+                <option selected="selected">Здание</option>
+                <option>Сотрудник</option>
+                <option>Администратор</option>
+            </select><br>
+            <button class="btn">Рассчитаь</button><br>
+            <input type="text" name="title" placeholder="Колличество" class="input_login response"><br>
+        </form>
+
+        <h2 class="add_title_dv">Площадь аудиторий по учебному заведению</h2>
+        <form method="post" class="form_add ">
+            <button class="btn">Рассчитать</button><br>
+            <input type="text" name="title" placeholder="Колличество" class="input_login response"><br>
+        </form>
+
+    </div>
+</div>
